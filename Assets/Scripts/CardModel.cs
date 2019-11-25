5531a1f831d5fd40c5ef82608b567aaa0daefdf8
yamamoto_yuta <40594324+yuta-yamamoto54@users.noreply.github.com>
2019-11-25 16:30:15 +0900

    カード表示用プログラム完成　Cards Game part2

---
+++ Assets/Scripts/CardModel.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardModel : MonoBehaviour
+{
+    // SpriteRendererクラスを参照します。
+    //①（Cardmodelクラスが直接アタッチされているオブジェクトのクラスを参照）
+
+    SpriteRenderer spriteRenderer;
+
+    public Sprite[] faces;
+    public Sprite cardBack;
+    public int cardIndex; // faces[cardIndex];
+
+    public void ToggleFace(bool showFace)
+    {
+
+
+        if (showFace)
+        {
+            // 表表示
+            //unity側で設定したfacesとcardBackから呼び出し描画
+            spriteRenderer.sprite = faces[cardIndex];
+        }
+        else
+        {
+            //裏表示
+            spriteRenderer.sprite = cardBack;
+
+        }
+    }
+
+    void Awake() 
+    {
+        //ゲームが始まる前の初期化
+        //このスクリプトがアタッチされているオブジェクトのインスペクター上のspriteRendererを取得します。
+        //①とセット。
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+}
---
+++ Assets/Scripts/DebugChangeCard.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebugChangeCard : MonoBehaviour
+{
+    //CardModelクラスの参照
+    CardModel cardModel;
+    int cardIndex = 0;
+
+    public GameObject card;
+
+    void Awake()
+    {
+        //cardにアタッチされているCardModelを取得
+        cardModel = card.GetComponent<CardModel>();
+    }
+
+    private void OnGUI()
+    {
+        //Hit meと書かれているボタンを作って押されたら実行
+        if (GUI.Button(new Rect(10, 10, 100, 20),"Hit me!!"))
+        {
+            if (cardIndex >= cardModel.faces.Length)
+            {
+                cardIndex = 0;
+                cardModel.ToggleFace(false);
+            }
+            else
+            {
+                cardModel.cardIndex = cardIndex;
+                cardModel.ToggleFace(true);
+            }
+
+            cardIndex++;
+
+        }
+    }
+}
